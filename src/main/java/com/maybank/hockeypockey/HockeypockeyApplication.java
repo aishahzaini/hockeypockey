bfe5aea620825590599a6d25309e3d90584ab733
aishahzaini <aishahzaini129@gmail.com>
2023-12-15 21:27:58 +0800

    first commit

---
+++ src/main/java/com/maybank/hockeypockey/HockeypockeyApplication.java
@@ -0,0 +1,13 @@
+package com.maybank.hockeypockey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HockeypockeyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HockeypockeyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maybank/hockeypockey/HockeypockeyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maybank.hockeypockey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HockeypockeyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
